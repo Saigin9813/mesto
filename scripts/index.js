e517e46d58f14370836d609800a8ebce84d74283
Saigin Sergei <Saigin9813@yandex.ru>
2023-03-31 13:01:13 +0500

    5 спринт проекта Mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,85 +1,65 @@
-let profileEditor = document.querySelector('.profile__editor');
-let profileName = document.querySelector('.profile__name');
-let profileProffesion = document.querySelector('.profile__proffesion');
-let profileAdd = document.querySelector('.profile__add');
+import { initialCards } from "./cards.js";
+
+const profileEditor = document.querySelector('.profile__editor');//кнопка редактирования профиля
+const profileName = document.querySelector('.profile__name');// Поле с именем на странице
+const profileProffesion = document.querySelector('.profile__proffesion');// Поле с профессией на стринце
+const mestoAdd = document.querySelector('.profile__add');//Кнопка добавления места
 
 
-let popups = document.querySelectorAll('.popup');
-let popupAddImage = document.querySelector('.popup_image');
-let popupEdit = document.querySelector('.popup_edit');
-let popupName = document.querySelector('.popup__profile');
-let popupProffesion = document.querySelector('.popup__proffesion');
+const popups = document.querySelectorAll('.popup');//Находим все popup
+
+const popupAddImage = document.querySelector('.popup__type_add-mesto');//Находим popup с добавлением карточек
+const popupEdit = document.querySelector('.popup__type_edit_profile');//Находим popup с измением профиля
+const popupZoom = document.querySelector('.popup__type_open-image');//Находим popup с увилечением изоброжения
+
+const popupProfile = document.querySelector('.popup__input_type-name');//Находим инпут с именем
+const popupProffesion = document.querySelector('.popup__input_type-proffession');//Находим инпут с профессией
+const nameMesto = document.querySelector('.popup__type_name-mesto');//Находим инпут с названием места
+const imageMesto = document.querySelector('.popup__type_image-mesto');//Находим инпут с добавлением ссылки на картинку
+
+const imageZoom = popupZoom.querySelector('.popup__img');// Поле с картинкой
+const zoomTtile = popupZoom.querySelector('.popup__zoom-title');// Поле с подписью к картинке
 
 
-let popupZoom = document.querySelector('.popup_zoom');
-let imageZoom = popupZoom.querySelector('.popup__img');
-let zoomTtile = popupZoom.querySelector('.popup__zoom-title');
+const formEdit = document.querySelector('.popup__content_type_edit_profile');///Ищем форму у popup с измением профиля
+const formMesto = document.querySelector('.popup__content_type_add-mesto');//Ищем форму у popup с добавленим места
+
+const element = document.querySelector('.element');//Ищем секцию куда будем вставлять картинки
 
 
-let buttonSave = document.querySelector('.popup__content');
-let buttonAddMesto = document.querySelector('.popup__content-mesto');
-let elementsTitle = document.querySelector('.elements__title');
-let elements = document.querySelector('.elements');
-
-let nameMesto = document.querySelector('.popup__name-mesto');
-let imageMesto = document.querySelector('.popup__image-mesto');
-
-
-// Открытия окна с редактрированиям профиля
-const popupOpen = () =>{
-  popupName.value = profileName.textContent;
-  popupProffesion.value = profileProffesion.textContent;
-  popupEdit.classList.add('popup_opened');
+// Функция открытия popup-ов
+const openPopup = (popup)=>{
+  popup.classList.add('popup_opened')
 }
-profileEditor.addEventListener('click',popupOpen);
-
-// Откртыия окна с добавлением места
-const popupOpenMesto = ()=>{
-  popupAddImage.classList.add('popup_opened');
-}
-profileAdd.addEventListener('click', popupOpenMesto);
-
 // Функция закрытия popup
-const popupClose = (popup) => {
+const closePopup = (popup) => {
   popup.classList.remove('popup_opened');
 }
+// Открытия окна с редактрированиям профиля
+const openPopupProfile = () =>{
+  popupProfile.value = profileName.textContent;
+  popupProffesion.value = profileProffesion.textContent;
+  openPopup(popupEdit);
+}
+profileEditor.addEventListener('click',openPopupProfile);
+
+// Откртыия окна с добавлением места
+const openPopupMesto = ()=>{
+  openPopup(popupAddImage);
+}
+mestoAdd.addEventListener('click', openPopupMesto);
+
 // Закрытия popup по нажатию крестик
-const closeIco = (popup)=>{
-popup.forEach((evt) => {
-let closeIco = evt.querySelector('.popup__close-button');
- closeIco.addEventListener('click', () => {
-   evt.classList.remove('popup_opened');
+const closePopupIco = (popup)=>{
+  popup.forEach((evt) => {
+  const closeIco = evt.querySelector('.popup__close-button');
+    closeIco.addEventListener('click', () => {
+    evt.classList.remove('popup_opened');
  })
 })}
-closeIco(popups);
+closePopupIco(popups);
 
 const cardsTemplate = document.querySelector('#elements-template').content;
-const initialCards = [
-  {
-    name: 'Архыз',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-    name: 'Челябинская область',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-    name: 'Иваново',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-    name: 'Камчатка',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-    name: 'Холмогорский район',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-    name: 'Байкал',
-    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
 // Функция удаления карточки
 const deleteCard = (button,template) =>{
   button.addEventListener('click', ()=>{
@@ -93,69 +73,69 @@ const activeLike =(likeItem)=> {
 })}
 
 // Функция клика на картинку и открытия ее в popup
-const  popupZoomOpen = (popup, image,title)=>{ 
+const  openPopupZoom = (popup, image,title)=>{ 
   image.addEventListener('click', () =>{
   popup.classList.add('popup_opened');
   imageZoom.src = image.src;
   zoomTtile.textContent = title.textContent;
 })}
-
+const addCard = (template) =>{
+  element.prepend(template);
+}
 // Функция добавление карточек из массива
 const addCards = (arr) => { 
-  arr.forEach((element) => {
+  arr.forEach((elements) => {
   // Клонируем tepmlate и заполняем его данными из массива
-  const elementsTemplate = cardsTemplate.querySelector('.elements__card').cloneNode(true);
-  elementsTemplate.querySelector('.elements__title').textContent = element.name;
-  elementsTemplate.querySelector('.elements__image').src = element.link;
+    const elementsTemplate = cardsTemplate.querySelector('.elements__card').cloneNode(true);
+    elementsTemplate.querySelector('.elements__title').textContent = elements.name;
+    elementsTemplate.querySelector('.elements__image').src = elements.link;
+    elementsTemplate.querySelector('.elements__image').alt = elements.name;
 
   // Осуществляем поиск нужных элементов в сохданных карточках
-  let image = elementsTemplate.querySelector('.elements__image');
-  let title = elementsTemplate.querySelector('.elements__title');
-  let deleteCardsButton = elementsTemplate.querySelector('.cards__delete');
-  let likeActive = elementsTemplate.querySelector('.elements__like');
-  elements.append(elementsTemplate);
+    const image = elementsTemplate.querySelector('.elements__image');
+    const title = elementsTemplate.querySelector('.elements__title');
+    const deleteCardsButton = elementsTemplate.querySelector('.cards__delete');
+    const likeActive = elementsTemplate.querySelector('.elements__like');
+    element.append(elementsTemplate);
 
-   deleteCard(deleteCardsButton,elementsTemplate);
-   activeLike(likeActive);
-   popupZoomOpen(popupZoom,image,title);
+    deleteCard(deleteCardsButton,elementsTemplate);
+    activeLike(likeActive);
+    openPopupZoom(popupZoom,image,title);
 })
 };
 // Функция добавления карточки
-const addCard = (template) =>{
-  elements.prepend(template);
-}
 
 addCards(initialCards);
 
 // Функция изменения профиля 
-const btnSaveProfile = (event) => {
+const saveProfileButton = (event) => {
   event.preventDefault();
-  profileName.textContent = popupName.value;
+  profileName.textContent = popupProfile.value;
   profileProffesion.textContent = popupProffesion.value;
-  popupClose(popupEdit);
+  closePopup(popupEdit);
 };
-buttonSave.addEventListener('submit',btnSaveProfile);
+formEdit.addEventListener('submit',saveProfileButton);
 
 
-const btnSaveMesto = (event) =>{
+const addMestoButton = (event) =>{
   event.preventDefault();
-  let getElement = cardsTemplate.querySelector('.elements__card').cloneNode(true);
-  let image = getElement.querySelector('.elements__image');
-  let title = getElement.querySelector('.elements__title');
-  let deleteCardsButton = getElement.querySelector('.cards__delete');
-  let likeActive = getElement.querySelector('.elements__like');
+  const getElement = cardsTemplate.querySelector('.elements__card').cloneNode(true);
+  const image = getElement.querySelector('.elements__image');
+  const title = getElement.querySelector('.elements__title');
+  const deleteCardsButton = getElement.querySelector('.cards__delete');
+  const likeActive = getElement.querySelector('.elements__like');
   image.src = imageMesto.value;
+  image.alt = nameMesto.value;
   title.textContent = nameMesto.value;
 
   deleteCard(deleteCardsButton,getElement);
   activeLike(likeActive);
-  popupZoomOpen(popupZoom,image,title);
+  openPopupZoom(popupZoom,image,title);
   addCard(getElement);
-  popupClose(popupAddImage);
-  imageMesto.value = '';
-  nameMesto.value = '';
+  closePopup(popupAddImage);
+  formMesto.reset();
 }
-buttonAddMesto.addEventListener('submit',btnSaveMesto);
+formMesto.addEventListener('submit',addMestoButton);
 
 
 
